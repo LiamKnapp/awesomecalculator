450823431e38a79019c658b6c2cab3d4e1d9a900
Liam Knapp <Lknapp1110@conestogac.on.ca>
2023-11-10 16:41:41 -0500

    trigger

--- src/setup.py
+++ src/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dedreira-awesomecalculator", # Replace with your own username
+    name="LiamKnapp-awesomecalculator", # Replace with your own username
     version= "1.0.0",
-    author="Daniel Edreira",
-    author_email="dedreira@github.com",
+    author="Liam",
+    author_email="lknapp1110@conestogac.on.ca",
     description="An awesome calculator package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dedreira/awesomecalculator",
+    url="https://github.com/LiamKnapp/awesomecalculator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
